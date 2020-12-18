96d5f2befb32195091daac427e231b7935b5842f
vitaliy28073 <vikkttt@mail.ru>
2020-12-18 13:12:15 +0300

    commit

--- js/accordeon.js
+++ js/accordeon.js
@@ -1,32 +1,32 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    document.querySelectorAll('#js-section')
-        .forEach(function (section) {
-            section.addEventListener('click', function (e) {
-                document.querySelectorAll('#js-section').forEach(function (se) {
-                    se.querySelector('#js-content').style.maxHeight = '0px'
-                })
-
-                const accordeonSection = e.target.closest('#js-section')
-                const insideElHeight = accordeonSection.querySelector('#js-content > *').clientHeight
-                accordeonSection.querySelector('#js-content').style.maxHeight = insideElHeight + 'px'
-
-                const accordionsElSection = document.querySelectorAll('#js-section');
-
-                if (this.classList.contains('active')) {
-
-                    document.querySelectorAll('#js-section').forEach(function (section) {
-                        section.querySelector('#js-content').style.maxHeight = '0px'
-                    })
-                    this.classList.remove('active');
-                } else {
-                    for (el of accordionsElSection) {
-                        el.classList.remove('active');
-                    }
-                    this.classList.add('active');
-                }
-            })
+  document.querySelectorAll('.js-section')
+    .forEach(function (section) {
+      section.addEventListener('click', function (e) {
+        document.querySelectorAll('.js-section').forEach(function (se) {
+          se.querySelector('.js-content').style.maxHeight = '0px'
         })
+
+        const accordeonSection = e.target.closest('.js-section')
+        const insideElHeight = accordeonSection.querySelector('.js-content > *').clientHeight
+        accordeonSection.querySelector('.js-content').style.maxHeight = insideElHeight + 'px'
+
+        const accordionsElSection = document.querySelectorAll('.js-section');
+
+        if (this.classList.contains('active')) {
+
+          document.querySelectorAll('.js-section').forEach(function (section) {
+            section.querySelector('.js-content').style.maxHeight = '0px'
+          })
+          this.classList.remove('active');
+        } else {
+          for (el of accordionsElSection) {
+            el.classList.remove('active');
+          }
+          this.classList.add('active');
+        }
+      })
+    })
 
 
 })
