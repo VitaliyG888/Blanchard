8e4e92e7a8bea7aafd26b68e5abe7a56c27faa56
vitaliy28073 <vikkttt@mail.ru>
2020-12-18 06:45:54 +0300

    commit

--- js/tab.js
+++ js/tab.js
@@ -1,28 +1,28 @@
 let tab = function () {
-    let tabNav = document.querySelectorAll('.tab-nav__link');
+  let tabNav = document.querySelectorAll('.tab-nav__link');
 
-    let tabContent = document.querySelectorAll('.tab');
+  let tabContent = document.querySelectorAll('.tab');
 
-    let tabName;
+  let tabName;
 
+  tabNav.forEach(item => {
+    item.addEventListener('click', selectTabNav)
+  });
+
+  function selectTabNav() {
     tabNav.forEach(item => {
-        item.addEventListener('click', selectTabNav)
+      item.classList.remove('is-active');
     });
+    this.classList.add('is-active');
+    tabName = this.getAttribute('data-tab-name');
+    selectTabContent(tabName);
+  }
 
-    function selectTabNav() {
-        tabNav.forEach(item => {
-            item.classList.remove('is-active');
-        });
-        this.classList.add('is-active');
-        tabName = this.getAttribute('data-tab-name');
-        selectTabContent(tabName);
-    }
-
-    function selectTabContent(tabName) {
-        tabContent.forEach(item => {
-            item.classList.contains(tabName) ? item.classList.add('is-active__description') : item.classList.remove('is-active__description');
-        })
-    }
+  function selectTabContent(tabName) {
+    tabContent.forEach(item => {
+      item.classList.contains(tabName) ? item.classList.add('is-active__description') : item.classList.remove('is-active__description');
+    })
+  }
 
 };
 
