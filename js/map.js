419b7eaa7a2e8bfdb4dad44bd2b3f7a06a449c8b
vitaliy28073 <vikkttt@mail.ru>
2021-01-25 01:25:40 +0300

    commit

--- js/map.js
+++ js/map.js
@@ -1,19 +1,29 @@
-ymaps.ready(function () {
-	var myMap = new ymaps.Map("YMapsID", {
-		center: [55.758747, 37.601187],
-		zoom: 17,
-		scroll: false,
-		controls: []
-	});
+let flag = 0;
 
-	var myPlacemark = new ymaps.Placemark([55.758463, 37.601079], {}, {
-		iconLayout: 'default#image',
-		iconImageHref: '../image/location.png',
-		iconImageSize: [30, 42],
-		iconImageOffset: [-3, -42]
-	});
+window.addEventListener('scroll', function () {
+	let scrollY = window.scrollY;
+	let mapOffset = document.querySelector('#YMapsID').offsetTop;
+	if ((scrollY >= mapOffset - 500) && (flag == 0)) {
+		ymaps.ready(function () {
+			var myMap = new ymaps.Map("YMapsID", {
+				center: [55.758747, 37.601187],
+				zoom: 17,
+				scroll: false,
+				controls: []
+			});
 
-	myMap.geoObjects.add(myPlacemark); {
-		searchControlProvider: 'yandex#search'
+			var myPlacemark = new ymaps.Placemark([55.758463, 37.601079], {}, {
+				iconLayout: 'default#image',
+				iconImageHref: '../image/location.png',
+				iconImageSize: [30, 42],
+				iconImageOffset: [-3, -42]
+			});
+
+			myMap.geoObjects.add(myPlacemark); {
+				searchControlProvider: 'yandex#search'
+			}
+		});
+
+		flag = 1;
 	}
-});
+})
